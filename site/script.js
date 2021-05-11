231e00b07a4e29f324bacae612dc81f56625b76e
Mauricio Casagrande <mauricio.araras@hotmail.com>
2021-05-11 20:46:34 -0300

    att

--- site/script.js
+++ site/script.js
@@ -1,8 +1,7 @@
 $(document).ready(function () {  
-    var escolhaSalgado = $('#salgado').val();
 
     $("#botao").click(function(){
-        alert(escolhaSalgado);
+        alert($("#salgado").val());
     });
         
    });
