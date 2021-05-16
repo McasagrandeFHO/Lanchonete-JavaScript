0bdaf2343b45d0b32d6b2b4df85d3c240b5eaa36
Mauricio Casagrande <mauricio.araras@hotmail.com>
2021-05-16 10:16:56 -0300

    Att

--- site/script.js
+++ site/script.js
@@ -1,7 +1,36 @@
 $(document).ready(function () {  
 
     $("#botao").click(function(){
-        alert($("#salgado").val());
+        $valorSalgado = $("#salgado").val();
+        if($valorSalgado == "coxinha") {
+            $valorSalgado = 4.00;
+        }
+        else if($valorSalgado == "hamburger") {
+            $valorSalgado = 5.0;
+        }
+        else if($valorSalgado == "esfirra") {
+            $valorSalgado = 4.50;
+        }    
+        else if($valorSalgado == "churrasco") {
+            $valorSalgado = 4.50;
+        }    
+        
+
+        $valorBebida = $("#bebida").val();
+        if($valorSalgado == "coca") {
+            $valorSalgado = 5.00;
+        }
+        else if($valorSalgado == "pepsi") {
+            $valorSalgado = 5.0;
+        }
+        else if($valorSalgado == "guarana") {
+            $valorSalgado = 5.50;
+        }    
+        else if($valorSalgado == "suco") {
+            $valorSalgado = 4.00;
+        }    
+
+
     });
         
    });
